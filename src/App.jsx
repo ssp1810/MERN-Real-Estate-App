35f308e321732c7244506fdc32cf61e61c16761f
ssp <shreya23kp@gmail.com>
2024-03-04 12:25:34 +0530

    Added header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import SignUp from './pages/SignUp.jsx';
 import About from './pages/About.jsx';
 import Profile from './pages/Profile.jsx';
 import Signin from './pages/Signin.jsx';
+import Header from './components/Header.jsx';
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/sign-in' element={<Signin />}></Route>
