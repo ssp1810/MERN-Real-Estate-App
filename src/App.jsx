08aaefadc3a707ec36f02bae9c581489d3a80d84
ssp <shreya23kp@gmail.com>
2024-03-04 11:54:51 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx';
+import SignUp from './pages/SignUp.jsx';
+import About from './pages/About.jsx';
+import Profile from './pages/Profile.jsx';
+import Signin from './pages/Signin.jsx';
 
 export default function App() {
   return (
     <>
-      <h1 className="text-red-500">
-        App
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/sign-in' element={<Signin />}></Route>
+          <Route path='/sign-up' element={<SignUp />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 };
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Signin() {
+  return (
+    <h1>Sign in</h1>
+  )
+}
