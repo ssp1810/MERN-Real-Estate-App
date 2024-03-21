77c0cd0ef8e007f7de6494a7c0239338d5a904f2
ssp <shreya23kp@gmail.com>
2024-03-21 11:41:24 +0530

    create a middleware and a function to handle possible error

--- api/index.js
+++ api/index.js
@@ -1,30 +1,41 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL)
-      .then(() => {
-            console.log('Connected to MongoDB');
-      })
-      .catch((err) => {
-            console.log(err);
-      })
+mongoose
+     .connect(process.env.MONGO_URL)
+     .then(() => {
+          console.log("Connected to MongoDB");
+     })
+     .catch((err) => {
+          console.log(err);
+     });
 
 const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-      console.log('Server is running on the port 3000');
-})
-
-app.get('/test', (req, res) => {
-      res.json({
-            message: "Hello world!",
-      });
+     console.log("Server is running on the port 3000");
 });
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
+app.get("/test", (req, res) => {
+     res.json({
+          message: "Hello world!",
+     });
+});
+
+//middlewares
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+     const statusCode = err.statusCode || 500;
+     const message = err.message || "Internal server error";
+     return res.status(statusCode).json({
+          success: false,
+          statusCode: statusCode,
+          message: message,
+     });
+});
