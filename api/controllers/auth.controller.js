fb1444b56f093ab3c0df647c41c31da53eb0549f
ssp <shreya23kp@gmail.com>
2024-03-26 12:25:20 +0530

    completed sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
      const { username, email, password } = req.body;
@@ -8,6 +9,32 @@ export const signup = async (req, res, next) => {
      try {
           await newUser.save();
           res.status(201).json("User created successfully");
+     } catch (error) {
+          next(error);
+     }
+};
+
+export const signin = async (req, res, next) => {
+     const { email, password } = req.body;
+     try {
+          const validUser = await User.findOne({ email });
+          if (!validUser) {
+               return next(errorHandler(404, "User not found!"));
+          }
+          const validPassword = await bcrypt.compareSync(
+               password,
+               validUser.password
+          );
+          if (!validPassword) {
+               return next(errorHandler(401, "Wrong Credentials"));
+          }
+          const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+          //no need to show password, show data except password
+          const { password: pass, ...rest } = validUser._doc;
+
+          res.cookie("access_token", token, { httpOnly: true })
+               .status(200)
+               .json(rest);
      } catch (error) {
           next(error);
      }
