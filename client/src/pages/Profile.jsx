1c414940c0042926e5c0d1e5c16a4288008fe755
ssp <shreya23kp@gmail.com>
2024-03-27 16:00:58 +0530

    Complete Profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
-
+import React from "react";
+import { useSelector } from "react-redux";
 export default function Profile() {
-  return (
-      <h1>Profile</h1>
-  )
+     const { currentUser } = useSelector((state) => state.user);
+     return (
+          <div className="p-3 max-w-lg mx-auto">
+               <h1 className="text-3xl font-semibold text-center my-7">
+                    {currentUser.name}
+               </h1>
+               <form className="flex flex-col gap-4">
+                    <img
+                         src={currentUser.avatar}
+                         alt="profile"
+                         className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+                    />
+                    <input
+                         id="username"
+                         type="text"
+                         placeholder="Username"
+                         className="border p-3 rounded-lg"
+                    />
+                    <input
+                         id="email"
+                         type="text"
+                         placeholder="Email"
+                         className="border p-3 rounded-lg"
+                    />
+                    <input
+                         id="password"
+                         type="text"
+                         placeholder="Password"
+                         className="border p-3 rounded-lg"
+                    />
+                    <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-80">
+                         Update Profile
+                    </button>
+               </form>
+               <div className="flex justify-between mt-5">
+                    <span className="text-red-700 cursor-pointer">
+                         Delete account
+                    </span>
+                    <span className="text-red-700 cursor-pointer">
+                         Sign out
+                    </span>
+               </div>
+          </div>
+     );
 }
