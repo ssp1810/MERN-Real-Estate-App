fd3f1428623ce8b5bcc096140340eb8d60c34c80
ssp <shreya23kp@gmail.com>
2024-03-27 15:45:13 +0530

    Update header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx';
-import SignUp from './pages/SignUp.jsx';
-import About from './pages/About.jsx';
-import Profile from './pages/Profile.jsx';
-import Signin from './pages/Signin.jsx';
-import Header from './components/Header.jsx';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+import Signin from "./pages/Signin.jsx";
+import Header from "./components/Header.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 export default function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/sign-in' element={<Signin />}></Route>
-          <Route path='/sign-up' element={<SignUp />}></Route>
-          <Route path='/about' element={<About />}></Route>
-          <Route path='/profile' element={<Profile />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
-};
-
+     return (
+          <>
+               <BrowserRouter>
+                    <Header />
+                    <Routes>
+                         <Route path="/" element={<Home />}></Route>
+                         <Route path="/sign-in" element={<Signin />}></Route>
+                         <Route path="/sign-up" element={<SignUp />}></Route>
+                         <Route path="/about" element={<About />}></Route>
+                         <Route element={<PrivateRoute />}>
+                              <Route
+                                   path="/profile"
+                                   element={<Profile />}
+                              ></Route>
+                         </Route>
+                    </Routes>
+               </BrowserRouter>
+          </>
+     );
+}
