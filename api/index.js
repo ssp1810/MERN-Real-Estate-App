b0fec2cfb07cee9aa67c0d50e7650b405ee11599
ssp <shreya23kp@gmail.com>
2024-05-01 18:53:07 +0530

    fix: solved the mistake in api

--- api/index.js
+++ api/index.js
@@ -37,10 +37,10 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+     res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 app.use((err, req, res, next) => {
