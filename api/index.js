0897c1294a86a060ff3261a7eae33d56fcf7a49c
ssp <shreya23kp@gmail.com>
2024-05-01 18:08:00 +0530

    added pre-installation for build

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 dotenv.config();
 
 mongoose
@@ -34,6 +36,12 @@ app.get("/test", (req, res) => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.use((err, req, res, next) => {
      const statusCode = err.statusCode || 500;
