89c190c57b3cc67575bc17141cfca3eeeb0b5b9d
ssp <shreya23kp@gmail.com>
2024-05-02 18:25:20 +0530

    Fixed the ES module error

--- api/index.js
+++ api/index.js
@@ -18,6 +18,7 @@ mongoose
           console.log(err);
      });
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -37,7 +38,7 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
 app.get("*", (req, res) => {
      res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
