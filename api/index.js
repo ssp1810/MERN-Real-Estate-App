ed87836370a17ef40a47166281f8ed11cd0681f8
ssp <shreya23kp@gmail.com>
2024-05-07 14:46:07 +0530

    fix: error resolving

--- api/index.js
+++ api/index.js
@@ -41,7 +41,7 @@ app.use("/api/listing", listingRouter);
 app.use(express.static(path.join(__dirname, "client", "dist")));
 
 app.get("*", (req, res) => {
-     res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+     res.sendFile(path.join(__dirname, "client", "dist", "index.js"));
 });
 
 app.use((err, req, res, next) => {
